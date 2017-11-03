16b7ab86462ac4a761672342a8f6331f3bb24271
Joan Caron <caronjoan@gmail.com>
2017-11-03 12:22:06 +0100

    empty projects

---
+++ src/CryptoCompare.WebSocket/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CryptoCompare.WebSocket.Tests")]
+[assembly: InternalsVisibleTo("CryptoCompare.WebSocket.Tests.Integration")]
---
+++ src/CryptoCompare/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("CryptoCompare.Tests")]
+[assembly:InternalsVisibleTo("CryptoCompare.Tests.Integration")]
+[assembly: InternalsVisibleTo("CryptoCompare.WebSocket.Tests")]
+[assembly: InternalsVisibleTo("CryptoCompare.WebSocket.Tests.Integration")]

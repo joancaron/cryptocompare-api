a54ecd36fb6b21f651babd5863f21c207c95b805
Joan Caron <caronjoan@gmail.com>
2017-11-04 20:27:10 +0100

    base exception

---
+++ src/CryptoCompare/Exceptions/CryptoCompareException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+
+using CryptoCompare.Responses;
+
+namespace CryptoCompare.Exceptions
+{
+    /// <summary>
+    /// Global Exception for signalling cryptocompare api errors.
+    /// </summary>
+    /// <seealso cref="T:System.Exception"/>
+    public class CryptoCompareException : HttpRequestException
+    {
+        /// <summary>
+        /// Initializes a new instance of the CryptoCompare.Exceptions.CryptoCompareException
+        /// class.
+        /// </summary>
+        /// <param name="apiResponseObject">The API response object.</param>
+        public CryptoCompareException(BaseApiResponse apiResponseObject)
+            : base($"{apiResponseObject.StatusType} - {apiResponseObject.StatusMessage}")
+        {
+        }
+    }
+}

d1790fefb79f09c12b2100396655ba3ada053f9d
Joan Caron <caronjoan@gmail.com>
2017-11-04 20:28:02 +0100

    root api urls

---
+++ src/CryptoCompare/Core/ApiUrls.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CryptoCompare.Core
+{
+    internal static class ApiUrls
+    {
+        public static readonly Uri MinApiEndpoint = new Uri("https://min-api.cryptocompare.com/api/data/", UriKind.Absolute);
+        public static readonly Uri SiteApiEndpoint = new Uri("https://www.cryptocompare.com/api/data/", UriKind.Absolute);
+    }
+}

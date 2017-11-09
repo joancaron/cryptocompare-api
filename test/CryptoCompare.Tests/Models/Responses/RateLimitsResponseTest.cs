19a872945f4a09e3d8e05b5c7dee1f4430b90166
Joan Caron <caronjoan@gmail.com>
2017-11-09 22:21:38 +0100

    RateLimitsResponse tests

---
+++ test/CryptoCompare.Tests/Models/Responses/RateLimitsResponseTest.cs
@@ -0,0 +1,31 @@
+﻿using CryptoCompare.Tests.Infrastructure;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace CryptoCompare.Tests.Models.Responses
+{
+    public class RateLimitsResponseTest
+    {
+        /// <summary>
+        /// Can deserialize RateLimitsResponse.
+        /// </summary>
+        [Fact]
+        public void Can_deserialize_RateLimitsResponse()
+        {
+            var model = TestHelper.ReadFixture("RateLimits").DeserializeJson<RateLimitResponse>();
+
+            model.IsSuccessfulResponse.Should().BeTrue();
+            model.CallsLeft.Should().NotBeNull();
+            model.CallsLeft.Price.Should().BePositive();
+            model.CallsLeft.Histo.Should().BePositive();
+            model.CallsLeft.News.Should().BePositive();
+
+            model.CallsMade.Should().NotBeNull();
+            model.CallsMade.Price.Should().BePositive();
+            model.CallsMade.Histo.Should().BePositive();
+            model.CallsMade.News.Should().BePositive();
+        }
+    }
+}

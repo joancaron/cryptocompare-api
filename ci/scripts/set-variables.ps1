352b4db9b10f3e5dd5d8f1a5798e40a650a0312b
Joan Caron <contact@joancaron.com>
2018-12-09 17:57:25 +0100

    hkl

--- ci/scripts/set-variables.ps1
+++ ci/scripts/set-variables.ps1
@@ -7,12 +7,9 @@ $version = $manifest.version
 
 if ($Env:BUILD_SOURCEBRANCH.StartsWith("refs/tags/v")) { 
     $isTriggeredByTag = $true
-    
 }else{
-    $commitId = & git rev-parse --short HEAD
-    $version = $manifest.version + '+' + $commitId
+    $version += '+' + $Env:BUILD_SOURCEVERSION.Substring(0, 6)
 }
 
 Write-Host "##vso[task.setvariable variable=isTriggeredByTag]$isTriggeredByTag"
 Write-Host "##vso[build.updatebuildnumber]$version"
-

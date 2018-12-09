01bb3050c6a15ece39c1a8d653dd2767f2638187
Joan Caron <contact@joancaron.com>
2018-12-09 19:59:08 +0100

    dasdsa

--- ci/scripts/set-variables.ps1
+++ ci/scripts/set-variables.ps1
@@ -2,15 +2,13 @@ Get-ChildItem Env:
 
 $manifest = Get-Content .\package.json | Out-String | ConvertFrom-Json
 
-$isPackable = $false
-$version = $manifest.version
+$isPackable = '$(Build.SourceBranch)'.StartsWith("refs/tags/v") -and $(Build.Reason) -ne 'PullRequest' -and $(System.PullRequest.IsFork) -ne $true
 
-if ($(Build.SourceBranch).StartsWith("refs/tags/v")
-    ) { 
-    $isPackable = $true
+if ($isPackable) { 
+    $version = $manifest.version
 }else{
-    $version += '+' + $Env:BUILD_SOURCEVERSION.Substring(0, 6)
+    $version = $manifest.version + '+' + $Env:BUILD_SOURCEVERSION.Substring(0, 6)
 }
 
-Write-Host "##vso[task.setvariable variable=isTriggeredByTag]$isTriggeredByTag"
+Write-Host "##vso[task.setvariable variable=isPackable]$isPackable"
 Write-Host "##vso[build.updatebuildnumber]$version"

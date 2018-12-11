69c7f387cd698638e510f73792d14ce32855ebf6
Joan Caron <caronjoan@gmail.com>
2018-12-11 22:05:58 +0100

    chore(azure-pipeline): Cross platform build

---
+++ ci/scripts/set-variables.ps1
@@ -0,0 +1,14 @@
+Get-ChildItem Env:
+
+$manifest = Get-Content .\package.json | Out-String | ConvertFrom-Json
+
+$canBePacked = '$(Build.SourceBranch)'.StartsWith("refs/tags/v") -and $(Build.Reason) -ne 'PullRequest' -and $(System.PullRequest.IsFork) -ne $true
+
+if ($canBePacked) { 
+    $version = $manifest.version
+}else{
+    $version = $manifest.version + '+' + $Env:BUILD_SOURCEVERSION.Substring(0, 6)
+}
+
+Write-Host "##vso[task.setvariable variable=canBePacked]$canBePacked"
+Write-Host "##vso[build.updatebuildnumber]$version"

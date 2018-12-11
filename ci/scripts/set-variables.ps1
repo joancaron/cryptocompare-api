5fb96f2aafe466691995cc5a2e106539832b7cb9
Joan Caron <joan@caron.work>
2018-12-11 21:34:42 +0100

    Update set-variables.ps1

--- ci/scripts/set-variables.ps1
+++ ci/scripts/set-variables.ps1
@@ -2,14 +2,14 @@ Get-ChildItem Env:
 
 $manifest = Get-Content .\package.json | Out-String | ConvertFrom-Json
 
-$isPackable = '$(Build.SourceBranch)'.StartsWith("refs/tags/v") -and $(Build.Reason) -ne 'PullRequest' -and $(System.PullRequest.IsFork) -ne $true
+$canBePacked = '$(Build.SourceBranch)'.StartsWith("refs/tags/v") -and $(Build.Reason) -ne 'PullRequest' -and $(System.PullRequest.IsFork) -ne $true
 
-if ($isPackable) { 
+if ($canBePacked) { 
     $version = $manifest.version
 }else{
     #$version = $manifest.version + '+' + $Env:BUILD_SOURCEVERSION.Substring(0, 6)
     $version = $manifest.version
 }
 
-Write-Host "##vso[task.setvariable variable=isPackable]$isPackable"
+Write-Host "##vso[task.setvariable variable=canBePacked]$canBePacked"
 Write-Host "##vso[build.updatebuildnumber]$version"

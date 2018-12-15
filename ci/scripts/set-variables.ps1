ac86a47106969feca72597d44d340e044cdfb35c
Joan Caron <contact@joancaron.com>
2018-12-15 13:31:21 +0100

    build condition

--- ci/scripts/set-variables.ps1
+++ ci/scripts/set-variables.ps1
@@ -1,6 +1,18 @@
 $manifest = Get-Content .\package.json | Out-String | ConvertFrom-Json
 
-$canBePacked = '$(Build.SourceBranch)'.StartsWith("refs/tags/v") -and '$(Build.Reason)' -ne 'PullRequest' -and '$(System.PullRequest.IsFork)' -ne 'True'
+$canBePacked = '$(Build.SourceBranch)'.StartsWith("refs/tags/v") -and '$(Build.Reason)' -ne 'PullRequest' -and '$(System.PullRequest.IsFork)' -ne $true
+
+Write-Host  'Packed?'
+Write-Host $canBePacked
+
+Write-Host 'Source branch'
+Write-Host '$(Build.SourceBranch)'.StartsWith("refs/tags/v")
+
+Write-Host 'Reason'
+Write-Host $(Build.Reason) -ne 'PullRequest' 
+
+Write-Host 'Forked'
+Write-Host  $(System.PullRequest.IsFork) -ne $true
 
 if ($canBePacked) { 
     $version = $manifest.version

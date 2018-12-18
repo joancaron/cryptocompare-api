e6ea51e3f292553abdc8556eceeabf66aa772ea5
Matthieu Le Berre <monsieur.leberre@gmail.com>
2018-12-18 07:59:52 +0000

    add integration test

---
+++ test/Cryptocompare.Integration.Tests/Core/ThrottledHttpClientHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using CryptoCompare;
+
+using Xunit;
+
+namespace Cryptocompare.Integration.Tests.Clients
+{
+    public class ThrottledHttpClientHandlerTests
+    {
+        [Fact]
+        public async Task WaitsBetweenQueries()
+        {
+            var throttleDelayMs = 200;
+            var queriesCount = 5;
+
+            var client = new CryptoCompareClient(throttleDelayMs: throttleDelayMs);
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            await Task.WhenAll(Enumerable.Repeat("start", queriesCount)
+               .Select(async _ => await client.RateLimits.CurrentHourAsync()));
+
+
+            stopWatch.Stop();
+
+            var minExpectedElapsedTime = queriesCount * throttleDelayMs;
+            Assert.True(stopWatch.ElapsedMilliseconds > minExpectedElapsedTime, $"Elapsed time should have been greater than {minExpectedElapsedTime}ms, but was {stopWatch.ElapsedMilliseconds}ms.");
+        }
+
+    }
+}
